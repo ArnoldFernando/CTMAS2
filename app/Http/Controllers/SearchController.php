b43d1ba30f72d4f42d2e0af815e6e659d56f8375
Arnold <junfernando727@gmail.com>
2024-06-29 17:52:40 +0800

    add search logic for faculty

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Faculty_and_staff;
 use App\Models\StudentList;
 use Illuminate\Http\Request;
 
@@ -20,5 +21,19 @@ class SearchController extends Controller
         })->get();
 
         return view('admin.student.list', compact('results'));
+    }
+
+    public function searchFaculty(Request $request)
+    {
+        $query = $request->input('query');
+
+        // Perform your search logic here using Eloquent ORM or other methods
+        $results = Faculty_and_staff::where(function($queryBuilder) use ($query) {
+            $queryBuilder->where('name', 'like', '%'.$query.'%')
+                        ->orWhere('faculty_id', 'like', '%'.$query.'%')
+                        ->orWhere('college', 'like', '%'.$query.'%');
+        })->get();
+
+        return view('admin.faculty.list', compact('results'));
     }
 }
