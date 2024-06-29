aa0a94258e819ad50522fa3305d3b9447a94c8e1
Arnold <junfernando727@gmail.com>
2024-06-29 17:55:29 +0800

    create model for faculty with protected fillable and add relationship to facultyrecords table

--- app/Models/Faculty_and_staff.php
+++ app/Models/Faculty_and_staff.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Faculty_and_staff extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'faculty_id', // Add 'student_id' to the fillable array
+        'name',
+        'college',
+        'image',
+    ];
+
+    public function facultyrecords()
+    {
+        return $this->hasMany(FacultyRecords::class, 'faculty_id', 'faculty_id');
+    }
 }
