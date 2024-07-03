2d8c090bb628bc863e648b67f3d0d348ddb3bb4d
SmokeCode00 <148770806+Smokecode00@users.noreply.github.com>
2024-07-03 23:58:21 +0800

    modify the display export pdf

--- resources/views/admin/student/export-records.blade.php
+++ resources/views/admin/student/export-records.blade.php
@@ -4,10 +4,19 @@
 
 <head>
     <title>Student Records</title>
+
     <style>
+        body {
+            font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+        }
+
         table {
             width: 100%;
             border-collapse: collapse;
+        }
+
+        thead {
+            text-align: center;
         }
 
         table,
@@ -21,37 +30,39 @@
             padding: 10px;
             text-align: left;
         }
+
+        .table-title {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
     </style>
 </head>
 
 <body>
-    <h1>Student Records</h1>
-    <table>
-        <thead>
+
+    <h3 class="table-title">Student Records</h3>
+    <table class="table table-bordered">
+        <thead class="table-dark">
             <tr>
-                <th>ID</th>
-                <th>Student ID</th>
+                <th>Date</th>
                 <th>Name</th>
                 <th>Course</th>
                 <th>College</th>
                 <th>Time In</th>
                 <th>Time Out</th>
-                <th>Created At</th>
-                <th>Updated At</th>
+                {{-- <th>Updated At</th> --}}
             </tr>
         </thead>
         <tbody>
             @foreach ($studentRecords as $record)
                 <tr>
-                    <td>{{ $record->id }}</td>
-                    <td>{{ $record->student_id }}</td>
+                    <td>{{ $record->created_at->format('F j, Y') }}</td>
                     <td>{{ $record->student->name }}</td>
                     <td>{{ $record->student->course }}</td>
                     <td>{{ $record->student->college }}</td>
                     <td>{{ \Carbon\Carbon::parse($record->time_in)->format('g:i:s A') }}</td>
                     <td>{{ \Carbon\Carbon::parse($record->time_out)->format('g:i:s A') }}</td>
-                    <td>{{ $record->created_at->format('Y-m-d') }}</td>
-                    <td>{{ $record->updated_at->format('Y-m-d') }}</td>
                 </tr>
             @endforeach
         </tbody>
