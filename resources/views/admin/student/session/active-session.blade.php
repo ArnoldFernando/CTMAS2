987cd299a948d18c2f9575a60255be9e5b6e4208
Arnold <junfernando727@gmail.com>
2024-07-03 12:55:33 +0800

    improved the ui of active session blade

--- resources/views/admin/student/session/active-session.blade.php
+++ resources/views/admin/student/session/active-session.blade.php
@@ -1,38 +1,45 @@
 <x-app-layout>
-    <div class="container p-4">
-        <div class="d-flex justify-content-center align-items-center">
-            <div class="w-100 max-w-4xl bg-white shadow rounded-lg overflow-auto" style="max-height: 24rem;">
-                <table class="table table-striped table-hover">
-                    <thead class="thead-dark sticky-top">
-                        <tr>
-                            <th>#</th> {{-- Sequential number column --}}
-                            <th>Student ID</th>
-                            <th>Name</th>
-                            <th>Course</th>
-                            <th>Action</th>
-                            <th>Time In</th>
-                            <th>Time Out</th>
-                            <th>Duration</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @php $counter = 1 @endphp {{-- Initialize counter --}}
-                        @foreach ($studentsTimedInToday as $student)
-                            <tr>
-                                <td>{{ $counter++ }}</td> {{-- Increment counter for each row --}}
-                                <td>{{ $student->student_id }}</td>
-                                <td>{{ $student->student->name }}</td>
-                                <td>{{ $student->student->course }}</td>
-                                <td>Active</td>
-                                <td>{{ $student->time_in }}</td>
-                                <td>{{ $student->time_out }}</td>
-
-                                <td>{{ $student->duration }}</td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+    @section('content_header')
+        <h5 class="fw-bold font"><i class="fa-solid fa-caret-right me-2 text-primary"></i>All Student Today</h5>
+    @stop
+    @section('content')
+        <div class="container-fluid">
+            <div class="bg-dark p-1 rounded-2 bg-opacity-25">
+                <div class="row">
+                    <div class="col">
+                        <table class="table table-bordered text-center font">
+                            <thead class="table-dark">
+                                <tr>
+                                    <th>No.1</th>
+                                    <th>Student ID</th>
+                                    <th>Name</th>
+                                    <th>Course</th>
+                                    <th>Action</th>
+                                    <th>Time In</th>
+                                    <th>Time Out</th>
+                                    <th>Duration</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @php $counter = 1 @endphp
+                                @foreach ($studentsTimedInToday as $student)
+                                    <tr>
+                                        <td>{{ $counter++ }}</td>
+                                        <td>{{ $student->student_id }}</td>
+                                        <td>{{ $student->student->name }}</td>
+                                        <td>{{ $student->student->course }}</td>
+                                        <td>Active</td>
+                                        <td>{{ \Carbon\Carbon::parse($student->time_in)->format('h:i A') }}</td>
+                                        <td>{{ \Carbon\Carbon::parse($student->time_out)->format('h:i A') }}</td>
+                                        <td>{{ $student->duration }}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
+    @stop
+
 </x-app-layout>
