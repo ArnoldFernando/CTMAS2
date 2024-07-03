c3822611944e510d357c421fe0f276a484481263
Arnold <junfernando727@gmail.com>
2024-07-03 12:57:47 +0800

    link the image of csu logo

--- resources/views/admin/student/session/start-session.blade.php
+++ resources/views/admin/student/session/start-session.blade.php
@@ -7,6 +7,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ url('css/style.css') }}">
+
+    {{--  FAVICON  --}}
+    <link rel="shortcut icon" href="{{ asset('IMG/csulogo.png') }}" type="image/x-icon">
     {{-- Sweet Alert CDN --}}
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     {{-- Google Fonts --}}
