2193bcf67412221268c398dca83fce10a00a8fa4
SmokeCode00 <148770806+Smokecode00@users.noreply.github.com>
2024-07-04 22:12:52 +0800

    modify the display of export data

--- resources/views/admin/student/export-records.blade.php
+++ resources/views/admin/student/export-records.blade.php
@@ -1,25 +1,25 @@
-<!-- resources/views/pdf/student_records.blade.php -->
 <!DOCTYPE html>
 <html>
 
 <head>
     <title>Student Records</title>
-
+    <link rel="shortcut icon" href="{{ asset('IMG/csulogo.png') }}" type="image/x-icon">
     <style>
         body {
             font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
             box-sizing: border-box;
+            margin: 0;
+            padding: 0;
         }
 
         p {
             text-align: center;
-            text
         }
 
         table {
             width: 100%;
             border-collapse: collapse;
-            margin-top: 0px;
+            /* margin-top: 20px; */
         }
 
         th,
@@ -27,110 +27,122 @@
             border: 1px solid black;
             padding: 8px;
             text-align: center;
-
+            font-size: 15px;
+            /* font-weight: 600; */
         }
 
         thead {
-            text-align: center;
-            margin-bottom: 2em;
-        }
+            background-color: #f2f2f2;
 
-        tbody {
-            text-align: center;
+
         }
 
         .title {
             background-color: #9DDE8B;
-            width: 100vw;
-            padding: 0;
+            width: 100%;
+            /* padding: 4px 0; */
         }
 
-        .div1 {
-            border-bottom: 1px black solid;
-            padding: 0;
-        }
-
+        .div1,
         .div2 {
-            border-top: 1px black solid;
+            border-top: 1px solid black;
             padding: 0;
         }
 
         @page {
-            margin: 100px 25px;
+            margin: 150px 25px;
         }
 
         header {
             position: fixed;
-            top: -80px;
+            top: -160px;
             left: 0px;
             right: 0px;
-            height: 50px;
+            height: 100px;
             text-align: center;
+            background-color: white;
+            padding: 10px 0;
         }
 
         footer {
             position: fixed;
-            bottom: -60px;
+            bottom: -100px;
             left: 0px;
             right: 0px;
             height: 50px;
             text-align: center;
+            background-color: white;
+            padding: 10px 0;
         }
 
         .pagenum:before {
             content: counter(page);
         }
+
+        .small-table td {
+            font-size: 12px;
+            padding: 5px;
+        }
+
+        .img {
+            margin-top: 10px;
+            width: 80px;
+            position: absolute;
+        }
+
+        img {
+            position: absolute;
+            width: 100%;
+            top: 13px;
+            left: 203px;
+        }
     </style>
+
 </head>
 
 <body>
     <header>
-        <p>CAGAYAN STATE UNIVERSITY <br>
-            APARRI CAMPUSES <br>
-            Maura, Aparri, Cagayan <br>
+        <div class="img">
+            <img src="IMG/library.png" alt="Absolute URL Image">
+        </div>
+        <p>
+            CAGAYAN STATE UNIVERSITY<br>
+            APARRI CAMPUSES<br>
+            Maura, Aparri, Cagayan
         </p>
-        {{-- <p>APARRI CAMPUSES</p>
-        <p>Maura, Aparri, Cagayan</p> --}}
-
+        <p class="title">LIBRARY SERVICES</p>
+        <p>ATTENDANCE FOR STUDENTS LOG SHEET</p>
     </header>
-    <p class="title">LIBRARY SERVICES</p>
-    <p>ATTENDANCE FOR STUDENTS LOG SHEET</p>
     <footer>
         <div class="div2">Footer Content Here</div>
         <div>Page <span class="pagenum"></span></div>
     </footer>
-
-
-
-
-
-    <table>
-        <thead>
-            <tr>
-                <th>Date</th>
-                <th>Name</th>
-                <th>Course</th>
-                <th>College</th>
-                <th>Time In</th>
-                <th>Time Out</th>
-                {{-- <th>Updated At</th> --}}
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($studentRecords as $record)
+    <main>
+        <table class="small-table">
+            <thead>
                 <tr>
-                    <td>{{ $record->created_at->format('F j, Y') }}</td>
-                    <td>{{ $record->student->name }}</td>
-                    <td>{{ $record->student->course }}</td>
-                    <td>{{ $record->student->college }}</td>
-                    <td>{{ \Carbon\Carbon::parse($record->time_in)->format('g:i A') }}</td>
-                    <td>{{ \Carbon\Carbon::parse($record->time_out)->format('g:i A') }}</td>
+                    <th>Date</th>
+                    <th>Name</th>
+                    <th>Course</th>
+                    <th>College</th>
+                    <th>Time In</th>
+                    <th>Time Out</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
-
-
+            </thead>
+            <tbody>
+                @foreach ($studentRecords as $record)
+                    <tr>
+                        <td>{{ $record->created_at->format('F j, Y') }}</td>
+                        <td>{{ $record->student->name }}</td>
+                        <td>{{ $record->student->course }}</td>
+                        <td>{{ $record->student->college }}</td>
+                        <td>{{ \Carbon\Carbon::parse($record->time_in)->format('g:i A') }}</td>
+                        <td>{{ \Carbon\Carbon::parse($record->time_out)->format('g:i A') }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </main>
 </body>
 
 </html>
