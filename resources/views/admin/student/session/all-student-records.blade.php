9bbbc2122a9b61cd4ab135983b13cf28a7337ada
SmokeCode00 <148770806+Smokecode00@users.noreply.github.com>
2024-07-05 22:06:08 +0800

    display the latest data

--- resources/views/admin/student/session/all-student-records.blade.php
+++ resources/views/admin/student/session/all-student-records.blade.php
@@ -69,7 +69,7 @@
                                 </thead>
                                 <tbody>
                                     @php $counter = 1 @endphp
-                                    @foreach ($sessionsByDay as $day => $sessions)
+                                    @foreach ($sessionsByDay->sortByDesc('created_at') as $day => $sessions)
                                         <tr>
                                             <td colspan="9" class="table-warning px-3 py-2">
                                                 {{ $day }}
