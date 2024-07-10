045be1b2c6027081b67d8aecb2df74ae0673fa9f
SmokeCode00 <148770806+Smokecode00@users.noreply.github.com>
2024-07-10 17:31:15 +0800

    Adding the Export per Colleges and Add the search buttton

--- resources/views/admin/student/session/all-student-records.blade.php
+++ resources/views/admin/student/session/all-student-records.blade.php
@@ -2,16 +2,49 @@
     @section('content_header')
         <h5 class="fw-bold font"><i class="fa-solid fa-caret-right me-2 text-primary"></i>Student Records</h5>
     @stop
-
+    @section('css')
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/css/bootstrap.min.css" rel="stylesheet">
+        <style>
+            /* Ensure dropdown menu is on top */
+            .dropdown-menu {
+                z-index: 1050;
+            }
+        </style>
+    @stop
     @section('content')
         <div class="container-fluid p-1 font">
             <div class="bg-secondary bg-opacity-50 rounded-2">
                 <div class="row">
                     <div class="col">
                         <div class="d-flex justify-content-end align-items-center py-1 px-1">
-                            <a href="{{ route('student-records.pdf') }}" class="btn btn-danger">
-                                <i class="fa-solid fa-file-pdf me-1"></i>Export PDF
-                            </a>
+                            <div class="dropdown">
+                                <button class="btn btn-danger dropdown-toggle" type="button" data-bs-toggle="dropdown"
+                                    aria-expanded="true">
+                                    <i class="fa-solid fa-file-pdf me-1"></i>Export PDF
+                                </button>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item bg-success" href="{{ route('student-records.pdf') }}"><i
+                                                class="fa-solid fa-caret-right me-2 text-white "></i>All
+                                            Records</a>
+                                    </li>
+                                    <li><a class="dropdown-item bg-orange" href="#"><i
+                                                class="fa-solid fa-caret-right me-2 text-white "></i>CICS</a></li>
+                                    <li><a class="dropdown-item bg-pink" href="#"><i
+                                                class="fa-solid fa-caret-right me-2 text-white "></i>CHM</a></li>
+                                    <li><a class="dropdown-item bg-primary" href="#"><i
+                                                class="fa-solid fa-caret-right me-2 text-white "></i>CTE</a></li>
+                                    <li><a class="dropdown-item bg-yellow" href="#"><i
+                                                class="fa-solid fa-caret-right me-2 text-white "></i>CBEA</a></li>
+                                    <li><a class="dropdown-item bg-secondary" href="#"><i
+                                                class="fa-solid fa-caret-right me-2 text-white "></i>CIT</a></li>
+                                    <li><a class="dropdown-item bg-info" href="#"><i
+                                                class="fa-solid fa-caret-right me-2 text-white "></i>CFAS</a></li>
+                                    <li><a class="dropdown-item bg-danger" href="#"><i
+                                                class="fa-solid fa-caret-right me-2 text-white "></i>CCJE</a></li>
+                                </ul>
+                            </div>
+                            <button class="btn btn-success ms-2"><i class="fa-solid fa-file-export me-1"></i>Export All
+                                Records</button>
                         </div>
                     </div>
                 </div>
@@ -24,15 +57,19 @@
                                 <input type="date" id="start_date" name="start_date" class="form-control px-3"
                                     value="{{ $startDate }}">
                             </div>
-                            <div class="form-group mb-2 col-md-5 text-dark">
+                            <div class="form-group mb-2 col-md-4 text-dark">
                                 <label for="end_date" class="mr-2">End Date:</label>
                                 <input type="date" id="end_date" name="end_date" class="form-control"
                                     value="{{ $endDate }}">
-                                <button type="submit" class="btn btn-primary ms-3">Filter</button>
+                                <button type="submit" class="btn btn-primary ms-1">Filter</button>
                             </div>
-                            {{-- <div class="form-group mb-2 col-md-2">
-                                <button type="submit" class="btn btn-primary">Filter</button>
-                            </div> --}}
+                            <div class="form-group mb-2 col-md-4 text-dark justify-content-end  ">
+                                {{-- <label for="end_date" class="mr-2">End Date:</label> --}}
+                                <input type="text" class="form-control" placeholder="Search here...">
+                                <button type="submit" class="btn btn-primary ms-1"><i
+                                        class="fa-solid fa-magnifying-glass me-1"></i>Search</button>
+                            </div>
+
                         </form>
                     </div>
                 </div>
@@ -107,5 +144,9 @@
                 </div>
             </div>
         </div>
+    @stop
+
+    @section('js')
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/js/bootstrap.bundle.min.js"></script>
     @stop
 </x-app-layout>
