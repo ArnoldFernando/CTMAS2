3ed84d8eeecaeeb75aa8b149c0f27d5954e15769
Mark Bryan Labinay <smokefacebook02@gmail.com>
2024-07-11 10:23:30 +0800

    modify the record table

--- resources/views/admin/faculty/all-faculty-records.blade.php
+++ resources/views/admin/faculty/all-faculty-records.blade.php
@@ -2,32 +2,91 @@
     @section('content_header')
         <h5 class="fw-bold font"><i class="fa-solid fa-caret-right me-2 text-primary"></i>Faculty Records</h5>
     @stop
-
+    @section('css')
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/css/bootstrap.min.css" rel="stylesheet">
+        <style>
+            /* Ensure dropdown menu is on top */
+            .dropdown-menu {
+                z-index: 1050;
+            }
+        </style>
+    @stop
     @section('content')
         <div class="container-fluid p-1 font">
             <div class="bg-secondary bg-opacity-50 rounded-2">
                 <div class="row">
                     <div class="col">
                         <div class="d-flex justify-content-end align-items-center py-1 px-1">
-                            <form method="GET" action="{{ route('faculty.records') }}" class="d-flex align-items-center">
-                                <div class="me-2">
-                                    <label for="start_date">Start Date:</label>
-                                    <input type="date" id="start_date" name="start_date" class="form-control"
-                                        value="{{ $startDate }}">
-                                </div>
-                                <div class="me-2">
-                                    <label for="end_date">End Date:</label>
-                                    <input type="date" id="end_date" name="end_date" class="form-control"
-                                        value="{{ $endDate }}">
-                                </div>
-                                <div class="me-2">
-                                    <button type="submit" class="btn btn-primary mt-4">Filter</button>
-                                </div>
-                            </form>
-                            <a href="{{ route('faculty-records.pdf') }}" class="btn btn-danger">
-                                <i class="fa-solid fa-file-pdf me-1"></i>Export PDF
+                            <div class="dropdown">
+                                <button class="btn btn-danger dropdown-toggle" type="button" data-bs-toggle="dropdown"
+                                    aria-expanded="true">
+                                    <i class="fa-solid fa-file-pdf me-1"></i>Export PDF
+                                </button>
+                                <ul class="dropdown-menu">
+                                    <li>
+                                        <form action="{{ route('faculty.records') }}" method="GET">
+                                            <div class="form-group px-2">
+                                                <label for="start_date">Start Date:</label>
+                                                <input type="date" name="start_date" id="start_date" class="form-control"
+                                                    required>
+                                            </div>
+                                            <div class="form-group px-2">
+                                                <label for="end_date">End Date:</label>
+                                                <input type="date" name="end_date" id="end_date" class="form-control"
+                                                    required>
+                                            </div>
+                                            <div class="form-group px-2">
+                                                <label for="college">College:</label>
+                                                <select name="college" id="college" class="form-control">
+                                                    <option value="">All Colleges</option>
+                                                    <option value="CICS">CICS</option>
+                                                    <option value="CHM">CHM</option>
+                                                    <option value="CTE">CTE</option>
+                                                    <option value="CBEA">CBEA</option>
+                                                    <option value="CIT">CIT</option>
+                                                    <option value="CFAS">CFAS</option>
+                                                    <option value="CCJE">CCJE</option>
+                                                </select>
+                                            </div>
+                                            <div class="form-group px-2 py-2">
+                                                <button type="submit" class="btn btn-primary w-100">
+                                                    <i class="fa-solid fa-file-pdf me-1"></i> Export PDF
+                                                </button>
+                                            </div>
+                                        </form>
+                                    </li>
+                                </ul>
+                            </div>
+                            <a href="{{ route('faculty-records.pdf') }}" class="btn btn-success ms-2">
+                                <i class="fa-solid fa-file-pdf me-1"></i>Makes Report
                             </a>
                         </div>
+                    </div>
+                </div>
+                <hr class="mt-0">
+                <div class="row">
+                    <div class="col-12">
+                        <form method="GET" action="{{ route('faculty.records') }}" class="form-inline">
+                            <div class="form-group mb-2 col-md-7 text-dark">
+                                <label for="start_date" class="mr-2">Start Date:</label>
+                                <input type="date" id="start_date" name="start_date" class="form-control px-3"
+                                    value="{{ $startDate }}">
+                                <div class="form-group ms-2">
+                                    <label for="end_date" class="mr-2">End Date:</label>
+                                    <input type="date" id="end_date" name="end_date" class="form-control"
+                                        value="{{ $endDate }}">
+                                    <button type="submit" class="btn btn-primary ms-1">Filter</button>
+                                </div>
+                            </div>
+
+                            <div class="form-group mb-2 col-md-5 text-dark justify-content-end  ">
+                                {{-- <label for="end_date" class="mr-2">End Date:</label> --}}
+                                <input type="text" class="form-control" placeholder="Search here...">
+                                <button type="submit" class="btn btn-primary ms-1"><i
+                                        class="fa-solid fa-magnifying-glass me-1"></i>Search</button>
+                            </div>
+
+                        </form>
                     </div>
                 </div>
                 <hr class="mt-0">
@@ -99,5 +158,8 @@
                 </div>
             </div>
         </div>
+    @stop
+    @section('js')
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/js/bootstrap.bundle.min.js"></script>
     @stop
 </x-app-layout>
