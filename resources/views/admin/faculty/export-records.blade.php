328493206b8c6ff840c44db4ba3a9caf84dd3465
Arnold <junfernando727@gmail.com>
2024-07-20 23:33:25 +0800

    change the export records blade

--- resources/views/admin/faculty/export-records.blade.php
+++ resources/views/admin/faculty/export-records.blade.php
@@ -1,59 +1,146 @@
-<!-- resources/views/pdf/student_records.blade.php -->
 <!DOCTYPE html>
 <html>
 
 <head>
     <title>Student Records</title>
+    <link rel="shortcut icon" href="{{ asset('IMG/csulogo.png') }}" type="image/x-icon">
     <style>
+        body {
+            font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+            box-sizing: border-box;
+            margin: 0;
+            padding: 0;
+        }
+
+        p {
+            text-align: center;
+        }
+
         table {
             width: 100%;
             border-collapse: collapse;
+            /* margin-top: 20px; */
         }
 
-        table,
         th,
         td {
             border: 1px solid black;
+            padding: 8px;
+            text-align: center;
+            font-size: 15px;
+            /* font-weight: 600; */
         }
 
-        th,
-        td {
-            padding: 10px;
-            text-align: left;
+        thead {
+            background-color: #f2f2f2;
+
+
+        }
+
+        .title {
+            background-color: #9DDE8B;
+            width: 100%;
+            /* padding: 4px 0; */
+        }
+
+        .div1,
+        .div2 {
+            border-top: 1px solid black;
+            padding: 0;
+        }
+
+        @page {
+            margin: 200px 25px 150px;
+        }
+
+        header {
+            position: fixed;
+            top: -160px;
+            left: 0px;
+            right: 0px;
+            height: 100px;
+            text-align: center;
+            background-color: white;
+            padding: 10px 0;
+        }
+
+        footer {
+            position: fixed;
+            bottom: -100px;
+            left: 0px;
+            right: 0px;
+            height: 50px;
+            text-align: center;
+            background-color: white;
+            padding: 10px 0;
+        }
+
+        .pagenum:before {
+            content: counter(page);
+        }
+
+        .small-table td {
+            font-size: 12px;
+            padding: 5px;
+        }
+
+        .img {
+            margin-top: 10px;
+            width: 80px;
+            position: absolute;
+        }
+
+        img {
+            position: absolute;
+            width: 100%;
+            top: 13px;
+            left: 203px;
         }
     </style>
+
 </head>
 
 <body>
-    <h1>Student Records</h1>
-    <table>
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Faculty ID</th>
-                <th>Name</th>
-                <th>College</th>
-                <th>Time In</th>
-                <th>Time Out</th>
-                <th>Created At</th>
-                <th>Updated At</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($facultyRecords as $record)
+    <header>
+        <div class="img">
+            <img src="IMG/library.png" alt="Absolute URL Image">
+        </div>
+        <p>
+            CAGAYAN STATE UNIVERSITY<br>
+            APARRI CAMPUSES<br>
+            Maura, Aparri, Cagayan
+        </p>
+        <p class="title">LIBRARY SERVICES</p>
+        <p>ATTENDANCE FOR STUDENTS LOG SHEET</p>
+    </header>
+    <footer>
+        <div class="div2">Footer Content Here</div>
+        <div>Page <span class="pagenum"></span></div>
+    </footer>
+    <main>
+        <table class="small-table">
+            <thead>
                 <tr>
-                    <td>{{ $record->id }}</td>
-                    <td>{{ $record->faculty_id }}</td>
-                    <td>{{ $record->faculty->name }}</td>
-                    <td>{{ $record->faculty->college }}</td>
-                    <td>{{ \Carbon\Carbon::parse($record->time_in)->format('g:i:s A') }}</td>
-                    <td>{{ \Carbon\Carbon::parse($record->time_out)->format('g:i:s A') }}</td>
-                    <td>{{ $record->created_at->format('Y-m-d') }}</td>
-                    <td>{{ $record->updated_at->format('Y-m-d') }}</td>
+                    <th>Date</th>
+                    <th>Name</th>
+                    <th>College</th>
+                    <th>Time In</th>
+                    <th>Time Out</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @foreach ($facultyRecords as $record)
+                    <tr>
+                        <td>{{ $record->created_at->format('F j, Y') }}</td>
+                        <td>{{ $record->faculty->name }}</td>
+                        <td>{{ $record->faculty->college }}</td>
+                        <td>{{ \Carbon\Carbon::parse($record->time_in)->format('g:i A') }}</td>
+                        <td>{{ \Carbon\Carbon::parse($record->time_out)->format('g:i A') }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </main>
 </body>
 
 </html>
