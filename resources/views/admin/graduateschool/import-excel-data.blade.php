5087ecac8fb3fb30a9e561da96693ad59770810e
Arnold <junfernando727@gmail.com>
2024-07-25 22:07:47 +0800

    create blade file for importing graduate school list as excel file

---
+++ resources/views/admin/graduateschool/import-excel-data.blade.php
@@ -0,0 +1,44 @@
+<x-app-layout>
+    <div class="container py-5 font">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card shadow-sm border-0">
+                    <div class="card-body p-5">
+                        <h1 class="mb-4">Insert Graduate List <small class="text-muted">(Excel file)</small></h1>
+                        <form action="{{ route('import.gradschool.data') }}" method="post" enctype="multipart/form-data">
+                            @csrf
+                            <div class="mb-3">
+                                <label for="file" class="form-label">Choose Excel File</label>
+                                <input type="file" required name="file" accept=".xls,.xlsx" class="form-control"
+                                    id="file">
+                            </div>
+                            <div class="d-flex justify-content-end">
+                                <button type="submit" class="btn btn-success">Import</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
+
+<style>
+    body {
+        background-color: #f8f9fa;
+    }
+
+    .card {
+        border-radius: 10px;
+    }
+
+    .btn-primary {
+        background-color: #007bff;
+        border-color: #007bff;
+    }
+
+    .btn-primary:hover {
+        background-color: #0056b3;
+        border-color: #004085;
+    }
+</style>
