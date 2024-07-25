d3b38d3872f059c49dc8db1d391fa4e494b6cfff
Arnold <junfernando727@gmail.com>
2024-07-25 22:04:21 +0800

    add importgradschooldata function to import the list of graduateschool

--- app/Http/Controllers/ImportDataController.php
+++ app/Http/Controllers/ImportDataController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Imports\importFacultyData;
+use App\Imports\importGradschoolData;
 use App\Imports\ImportStudentData;
 use Illuminate\Http\Request;
 use Maatwebsite\Excel\Facades\Excel;
@@ -29,5 +30,16 @@ class ImportDataController extends Controller
         Excel::import(new importFacultyData, $request->file('file'));
 
         return redirect()->route('faculty.list')->with('status', 'Imported successfully');
+    }
+
+    public function importGradschoolData(Request $request)
+    {
+
+        $request->validate([
+            'file' => 'required|file|mimes:xls,xlsx',
+        ]);
+        Excel::import(new importGradschoolData, $request->file('file'));
+
+        return redirect()->route('gradSchool.list')->with('status', 'Imported successfully');
     }
 }
