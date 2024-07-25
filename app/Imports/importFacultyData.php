a6f2b53bb6ad53695ff8f0706a320b92f2770e4f
Arnold <junfernando727@gmail.com>
2024-07-25 22:05:09 +0800

    change the variable to its coresponding name

--- app/Imports/importFacultyData.php
+++ app/Imports/importFacultyData.php
@@ -10,20 +10,20 @@ use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
 class importFacultyData implements ToModel, WithHeadingRow
 {
-   /**
-    * @param array $row
-    *
-    * @return \Illuminate\Database\Eloquent\Model|null
-    */
+    /**
+     * @param array $row
+     *
+     * @return \Illuminate\Database\Eloquent\Model|null
+     */
     public function model(array $row)
     {
         $facultyId = $row['faculty_id'];
 
         // Check if the student ID already exists in the database
-        $existingStudent = Faculty_and_staff::where('faculty_id', $facultyId)->first();
+        $existingFaculty = Faculty_and_staff::where('faculty_id', $facultyId)->first();
 
         // If the student ID doesn't exist, create a new record
-        if (!$existingStudent) {
+        if (!$existingFaculty) {
             return new Faculty_and_staff([
                 'faculty_id' => $row['faculty_id'],
                 'name' => $row['name'],
